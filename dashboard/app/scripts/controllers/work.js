68ad2271c72677419699eb07ab17d430e1747ac8
Chen <chen.song@wmich.edu>
2015-11-11 09:44:55 -0500

    update work files

--- dashboard/app/scripts/controllers/work.js
+++ dashboard/app/scripts/controllers/work.js
@@ -1,12 +1,40 @@
-var workApp = angular.module("WorkApp", []);
-var x;
+var workApp = angular.module("DashboardApp", []);
+
 workApp.controller("WorkCtrl", function($scope, $http){
+
+	var a={
+		author: "haziqhalimy",
+		comment: "174 comments",
+		id: "001",
+		image: "assets/images/work/1.jpg",
+		like: "12,774 likes",
+		title: "Journey in Central Park"
+	}
+
 	$http.get('http://localhost:3000/api/work').success(function(data, status, headers, config) {
-			$scope.contents=data.works;
-			x=$scope.contents;
-			console.log($scope.content);
-		}).error(function(data, status, headers, config) {
-			console.log("http get error.");
-		});
+		$scope.contents=data.works;
+	}).error(function(data, status, headers, config) {
+		console.log("http get error.");
+	});
+	
+	$scope.visible=false;
+	$scope.cardShow=true;
+	$scope.listShow=false;
+	$scope.showList=function() {
+		$scope.visible=!$scope.visible;
+	}
+	$scope.setList=function() {
+		$scope.cardShow=false;
+		$scope.listShow=true;
+		$scope.visible=!$scope.visible;
+	}
+	$scope.setCard=function() {
+		$scope.cardShow=true;
+		$scope.listShow=false;
+		$scope.visible=!$scope.visible;
+	}
+	$scope.addItem=function(data) {
+		($scope.contents).push(a);
+	}
 
 });
