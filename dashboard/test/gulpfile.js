f6db2ee949c134f360c9b28c1efb6bc6b8ab39dc
Chen <chen.song@wmich.edu>
2015-11-17 14:56:52 -0500

    install gulp packages

--- dashboard/test/gulpfile.js
+++ dashboard/test/gulpfile.js
@@ -1,42 +1,40 @@
 var gulp = require('gulp');
 
-var concat = require('gulp-concat');
-var cssmin = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var sass = require('gulp-sass');
-var uglify = require('gulp-uglify');
+
 
 // default task
-gulp.task('default', ['script', 'styles', 'watch'], function(){
+gulp.task('default', function(){
 	console.log("GULP IS WORKING!");
 });
 
-gulp.task('script', function(){
-	return gulp.src('./src/js/*.js')		
-		.pipe(gulp.dest('./dist/js/'))
-		.pipe(uglify())
-		.pipe(concat('app.js'))
-		.pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest('./dist/js/'));
+gulp.task('sass', function(){
+	
+
+
 });
 
-gulp.task('styles', function() {
-	return gulp.src('./src/sass/*.scss')
-		.pipe(sass())
-		.pipe(gulp.dest('./dist/css/'))
-		.pipe(cssmin())
-		.pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest('./dist/css/'));
+gulp.task('imagemin', function(){
+	
+
+	
+});
+
+gulp.task('clean', function(){
+	
+
+	
+});
+
+gulp.task('jshint', function(){
+	
+
+	
 });
 
 gulp.task('watch', function(){
-	gulp.watch('./src/js/*.js', ['script']);
-	gulp.watch('./src/sass/*.scss', ['styles']);
+
+
 });
-
-
-
-
 
 
 
