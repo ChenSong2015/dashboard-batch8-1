27202afdd2b5c94168e19f25ec33e8fbbe4450a9
liang121 <liangmoyu1@gmail.com>
2015-11-21 22:07:22 -0500

    pruducer

--- dashboard/app/scripts/controllers/producer.js
+++ dashboard/app/scripts/controllers/producer.js
@@ -1,4 +1,19 @@
 App.controller('producerCtrl', function ($scope, $window) {
-
+	$scope.search = function(){
+		document.getElementById("pac-input").value = "football field";
+		$scope.queryShow = true;
+	}
  })
+App.directive('s',function($timeout){
+	return{
+		scope:{},
+		restrict:'A',
+		link:function(scope,element,attrs){
+			element.bind("mouseover",function(){
+				angular.element("#pac-input").focus();
+			})
+			
+		}
+	}
+})
 
